20483313c85b603cf944a9b0d5189f07324ebdab
apop <thomas.beucher@recast.ai>
2017-08-11 16:35:30 +0200

    [UPD] add comments

--- atchoum2/generate_candidate.py
+++ atchoum2/generate_candidate.py
@@ -13,6 +13,8 @@ langue = 'fr-FR'
 
 def find_new_candidate(text, candidates, tracks):
   '''
+  Explore translation tracks and return the new sentence
+  when a new one is find.
 
   text - the text from which to generate new candidates
   candidates - the candidates which already exist
@@ -27,31 +29,40 @@ def find_new_candidate(text, candidates, tracks):
   return ''
 
 
-if os.path.isfile('stored_good_tracks'):
-  tool = language_check.LanguageTool(langue)
-  tracks = pickle.load(open('stored_good_tracks', 'rb'))
-  new_text = [my_args.text]
-  sizes = [len(new_text)]
-  for i in range(100):
-    if i > 3:
-      if sizes[-1] == sizes[-2] and sizes[-2] == sizes[-3]:
-        break
-    text = new_text[i]
-    added = True
-    while added:
-      n_text = find_new_candidate(text, new_text, tracks)
-      if n_text == '':
-        added = False
-        break
-      print('new: ', n_text)
-      if len(tool.check(n_text)) == 0:
-        print('--> Correct sentence')
-        new_text.append(n_text)
-      else:
-        print('--> Incorrect sentence')
-    sizes.append(len(new_text))
-    print('new element added (', sizes, ')')
-  for i, el in enumerate(new_text):
-    print('Candidate ', i, ': ', el)
-else:
-  print("No file store_good_tracks find!")
+def main():
+  '''
+  Find rephrase from the one given and then explore in
+  order to find new sentence from the ones which are
+  generated.
+
+  '''
+  if os.path.isfile('stored_good_tracks'):
+    tool = language_check.LanguageTool(langue)
+    tracks = pickle.load(open('stored_good_tracks', 'rb'))
+    new_text = [my_args.text]
+    sizes = [len(new_text)]
+    for i in range(100):
+      if i > 3:
+        if sizes[-1] == sizes[-2] and sizes[-2] == sizes[-3]:
+          break
+      text = new_text[i]
+      added = True
+      while added:
+        n_text = find_new_candidate(text, new_text, tracks)
+        if n_text == '':
+          added = False
+          break
+        print('new: ', n_text)
+        if len(tool.check(n_text)) == 0:
+          print('--> Correct sentence')
+          new_text.append(n_text)
+        else:
+          print('--> Incorrect sentence')
+      sizes.append(len(new_text))
+      print('new element added (', sizes, ')')
+    for i, el in enumerate(new_text):
+      print('Candidate ', i, ': ', el)
+  else:
+    print("No file store_good_tracks find!")
+
+main()
